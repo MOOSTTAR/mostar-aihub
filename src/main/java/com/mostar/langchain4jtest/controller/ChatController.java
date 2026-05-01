2030b3aea050c1c01d56e38124523174b03f6340
MOOSTTAR <2953370794@qq.com>
2026-05-01 23:59:26 +0800

    refactor: ChatController 导入语句优化
    - 移除通配符导入，改为显式导入
    - 移除未使用的 List 导入

--- src/main/java/com/mostar/langchain4jtest/controller/ChatController.java
+++ src/main/java/com/mostar/langchain4jtest/controller/ChatController.java
@@ -3,7 +3,12 @@ package com.mostar.langchain4jtest.controller;
 import java.io.IOException;
 
 import org.springframework.http.MediaType;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.mvc.method.annotation.SseEmitter;
 
 import com.mostar.langchain4jtest.aiservice.ConsultantService;
